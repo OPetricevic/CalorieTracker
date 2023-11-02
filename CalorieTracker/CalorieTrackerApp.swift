e8a4498e8f89c1f410e650a4224e5fede5536209
Omarep <omar.petricevic@gmail.com>
2023-11-02 10:14:45 +0100

    Initial Commit

---
+++ CalorieTracker/CalorieTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalorieTrackerApp.swift
+//  CalorieTracker
+//
+//  Created by Omar Petričević on 02.11.2023..
+//
+
+import SwiftUI
+
+@main
+struct CalorieTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
